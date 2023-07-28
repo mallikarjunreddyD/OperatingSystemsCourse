ad54b7c33d7f622125d8a56a84a37b9dea88505e
mallikarjunreddyD <arjun753016@gmail.com>
2023-07-28 12:17:57 +0530

    add simple fork

---
+++ System Calls/simplefork.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main() {
+    int ppid = getpid();
+    printf("Parent PID = %d\n",ppid);
+    fork();         // A new child process is created
+    printf("I will be executed twice");
+    printf("PID=%d\n",getpid());
+}
+
+
+// ps -ejH  : To check all the processes in the system 
+// ps -al   : To check all the user processes in the system
